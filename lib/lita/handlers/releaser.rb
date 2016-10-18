523b1e19145b00744c722bc21f575bba74b41eda
Josh Rendek <josh@bluescripts.net>
2016-10-18 10:45:42 -0400

    Fix bug with dash in project names, add ability to create releases

--- lib/lita/handlers/releaser.rb
+++ lib/lita/handlers/releaser.rb
@@ -1,6 +1,7 @@
 require 'json'
 require 'base64'
 require 'octokit'
+require 'pry'
 
 module Lita
   module Handlers
@@ -9,9 +10,28 @@ module Lita
       config :org
       config :token
 
-      route (/releaser ship (\w+)?/i), :ship, command: true, help: {
+      route (/releaser ship (\w|\S+)?/i), :ship, command: true, help: {
               'releaser ship <project_name>' => 'Merges develop into master for <project_name>'
             }
+
+      route (/releaser release (\w|\S+) (v[0-9]{1,}\.[0-9]{1,}\.[0-9]{1,}) (.*)/i), :release, command: true, help: {
+              'releaser release <project_name> <version> <release description>' => "Publishes a new release on github for <project_name> with <version> and <description>"
+            }
+
+      def release(response)
+        m = response.matches[0]
+        repo = m[0]
+        version = m[1]
+        description = m[2]
+        repo_path = "#{config.org}/#{repo}"
+
+        begin
+          resp = client.create_release(repo_path, version, {body: description, name: version})
+          response.reply "Release created for https://github.com/#{config.org}/#{repo}"
+        rescue => e
+          response.reply "Error: #{e}"
+        end
+      end
 
       def ship(response)
         repo = response.matches.first.last
